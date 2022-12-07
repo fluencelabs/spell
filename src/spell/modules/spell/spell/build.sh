764e93d2d3edebb64e427249f7903bfaaa740609
Anatoly Laskaris <github_me@nahsi.dev>
2022-12-07 11:02:22 +0200

    Reorganize cargo workspace

--- src/spell/modules/spell/spell/build.sh
+++ src/spell/modules/spell/spell/build.sh
@@ -9,15 +9,14 @@ marine build --release
 
 # copy .wasm to artifacts
 mkdir -p artifacts
-cp target/wasm32-wasi/release/spell.wasm artifacts/
+cp ../target/wasm32-wasi/release/spell.wasm artifacts/
 
 if [ ! -f "artifacts/sqlite3.wasm" ]; then
   # download SQLite 3
   curl -L https://github.com/fluencelabs/sqlite/releases/download/v0.15.0_w/sqlite3.wasm -o artifacts/sqlite3.wasm
 fi
 
-
 cd ./artifacts
 tar --exclude="spell.tar.gz" -f spell.tar.gz -zcv ./*
-mkdir -p ../spell-distro/spell-service
-cp -v spell.wasm sqlite3.wasm Config.toml ../spell-distro/spell-service/
+mkdir -p ../../spell-distro/spell-service
+cp -v spell.wasm sqlite3.wasm Config.toml ../../spell-distro/spell-service/
