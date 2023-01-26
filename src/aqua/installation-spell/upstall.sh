f1dbc1cc0d2ed8bc799a9898ab438f2313c1d915
folex <0xdxdy@gmail.com>
2023-01-26 18:30:55 +0700

    feat: store service id and blueprint id to KV

--- src/aqua/installation-spell/upstall.sh
+++ src/aqua/installation-spell/upstall.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+aqua --no-relay -i install.aqua -o air --air > /dev/null
+
 aqua run -i install.aqua \
-    -f 'upstall(app_config)' \
+    -f 'upstall_spell_to_relay(script, app_config)' \
     --plugin ./plugins \
-    --addr /ip4/127.0.0.1/tcp/9999/ws/p2p/12D3KooWBh6FcSJBQTCFLfiorVUuYKBiasyJgKLGdT7Ecp4HUyaz \
+    --addr /ip4/127.0.0.1/tcp/9999/ws/p2p/12D3KooWLwQvL1tPesXUgYoWF1dGgea5YSS3WipCHd6GAAErbvRR \
     --data '{
+        "script": "/Users/folex/Development/fluencelabs/spell/src/aqua/installation-spell/air/install.install.air",
         "app_config": {
             "basedir": "/Users/folex/Development/fluencelabs/examples/marine-examples/url-downloader/artifacts/",
             "services": [
