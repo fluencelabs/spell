31db4aabaf7525f9fbbbe44c3bdb932229977c1a
folex <0xdxdy@gmail.com>
2023-02-08 19:58:13 +0700

    chore: restructure

--- src/aqua/installation-spell/src/scripts/install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-aqua run -i install.aqua \
-    -f "install_to_relay(\"$1\")" \
-    --plugin ./plugins \
-    --addr /ip4/127.0.0.1/tcp/9999/ws/p2p/12D3KooWBh6FcSJBQTCFLfiorVUuYKBiasyJgKLGdT7Ecp4HUyaz \
--- src/aqua/installation-spell/src/scripts/upstall.sh
+++ src/aqua/installation-spell/src/scripts/upstall.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
-aqua --no-relay -i install.aqua -o air --air > /dev/null
+CWD="$(dirname "$0")"
+# go to the project root
+cd "$CWD/../.."
 
-aqua run -i install.aqua \
+aqua --no-relay -i src/aqua -o src/air --air > /dev/null
+
+# node sk: 3LGQSnGbAor5i1sSNFAIBJw76AENxYV/at7VgRv4pI4=
+aqua run -i src/aqua/install.aqua \
     -f 'upstall_spell_to_relay(script, app_config)' \
-    --plugin ./plugins \
-    --addr /ip4/127.0.0.1/tcp/9999/ws/p2p/12D3KooWLwQvL1tPesXUgYoWF1dGgea5YSS3WipCHd6GAAErbvRR \
+    --plugin src/plugins \
+    --addr /ip4/127.0.0.1/tcp/9999/ws/p2p/12D3KooWJDdHPGakMsVH4UytjjxeV6qjgB1ci6qTFjrrM6fDRyUs \
+    --timeout 60000 \
     --data '{
-        "script": "/Users/folex/Development/fluencelabs/spell/src/aqua/installation-spell/air/install.install.air",
+        "script": "/Users/folex/Development/fluencelabs/spell/src/aqua/installation-spell/src/air/spell.install.air",
         "app_config": {
             "basedir": "/Users/folex/Development/fluencelabs/examples/marine-examples/url-downloader/artifacts/",
             "services": [
