95b7d9bf2671a02c62ad53bc50746d2047f7d1d6
folex <0xdxdy@gmail.com>
2023-02-15 20:47:45 +0700

    chore: improve deal_spell

--- src/aqua/installation-spell/src/scripts/upload_deploy.sh
+++ src/aqua/installation-spell/src/scripts/upload_deploy.sh
@@ -10,7 +10,7 @@ NOW=$(date +%s)
 
 # node sk: bL8RRGuBJEWSj4JKzLCUgR/EY8+lit2g1LE2BE1oF/U=
 aqua run -i "$PROJECT/src/aqua/cli.aqua" \
-    --sk "8wJyRzI3K8NPlCGSf9E+6ExB5MBdXQAz7m0suStjgNg=" \
+    --sk "8wJyRzI3K8NPlCGSf9E+6ExB5MBdXQAz7m0jjjjjjNg=" \
     --show-config \
     -f 'upload_deploy(deploy_config)' \
     --plugin "$PROJECT/src/plugins" \
@@ -18,7 +18,6 @@ aqua run -i "$PROJECT/src/aqua/cli.aqua" \
     --timeout 60000 \
     --data '{
         "deploy_config": {
-            "deal_id": "prosh-is-the-beast",
             "installation_script": '"$(cat /Users/folex/Development/fluencelabs/spell/src/aqua/installation-spell/src/air/deal_spell.deal_install.air | jq -Rs)"',
             "installation_trigger": {
                 "clock": { "start_sec": 1676293670, "end_sec": 0, "period_sec": 600 },
@@ -36,15 +35,15 @@ aqua run -i "$PROJECT/src/aqua/cli.aqua" \
                                 "modules": [
                                     {
                                         "wasm": "/Users/folex/Development/fluencelabs/examples/marine-examples/url-downloader/artifacts/curl_adapter.wasm",
-                                        "config": "/Users/folex/Development/fluencelabs/examples/marine-examples/url-downloader/artifacts/curl_adapter.json"
+                                        "config": '"$(cat /Users/folex/Development/fluencelabs/examples/marine-examples/url-downloader/artifacts/curl_adapter.json | jq -Rs)"'
                                     },
                                     {
                                         "wasm": "/Users/folex/Development/fluencelabs/examples/marine-examples/url-downloader/artifacts/local_storage.wasm",
-                                        "config": "/Users/folex/Development/fluencelabs/examples/marine-examples/url-downloader/artifacts/local_storage.json"
+                                        "config": '"$(cat /Users/folex/Development/fluencelabs/examples/marine-examples/url-downloader/artifacts/local_storage.json | jq -Rs)"'
                                     },
                                     {
                                         "wasm": "/Users/folex/Development/fluencelabs/examples/marine-examples/url-downloader/artifacts/facade.wasm",
-                                        "config": "/Users/folex/Development/fluencelabs/examples/marine-examples/url-downloader/artifacts/facade.json"
+                                        "config": '"$(cat /Users/folex/Development/fluencelabs/examples/marine-examples/url-downloader/artifacts/facade.json | jq -Rs)"'
                                     }
                                 ]
                             }
