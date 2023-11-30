02898156e040b1fcbe40f395fef718ab8808315e
Maria Kuklina <101095419+kmd-fl@users.noreply.github.com>
2023-11-30 14:03:15 +0100

    feat(service,API): add list_remove_value [fixes NET-648] (#220)
    **Changes**:
    - Added `list_remove_value` that removes all entries of `value` in the list by the given `key`.
    - Renamed `list_index` to `list_order` name since this value now specifies the relative order of elements in the list, not their indices.
    - Fixed `list_pop_string` and `list_push_string` to rely on this `list_order` when evaluating indices rather than on the number of values in the list.

--- src/spell/modules/spell/spell/src/kv/collection.rs
+++ src/spell/modules/spell/spell/src/kv/collection.rs
@@ -11,11 +11,18 @@ pub fn list_push_string(key: &str, value: String) -> UnitValue {
     let result: eyre::Result<()> = try {
         let mut statement = conn.prepare(
             r#"
-                INSERT INTO kv (key, string, list_index)
+                INSERT INTO kv (key, string, list_order)
                     VALUES (
                         ?,
                         ?,
-                        (SELECT COUNT(*) FROM kv WHERE key = ?)
+                        COALESCE(
+                            (
+                                SELECT MAX(list_order) + 1
+                                FROM kv
+                                WHERE key = ?
+                            ),
+                            0
+                        )
                     )
             "#,
         )?;
@@ -35,23 +42,21 @@ pub fn list_pop_string(key: &str) -> StringValue {
     let result: eyre::Result<Option<String>> = try {
         let mut get = db.prepare(
             r#"
-            SELECT string, list_index FROM kv
+            SELECT string, max(list_order) FROM kv
                 WHERE key = ?
-                AND list_index = ((SELECT COUNT(*) FROM kv WHERE key = ?) - 1)
         "#,
         )?;
         get.bind(1, key)?;
-        get.bind(2, key)?;
 
         let mut result = None;
 
         if let State::Row = get.next()? {
             result = Some(get.read::<String>(0)?);
-            let list_index = get.read::<i64>(1)?;
+            let list_order = get.read::<i64>(1)?;
 
-            let mut delete = db.prepare(r#"DELETE FROM kv WHERE key = ? AND list_index = ?"#)?;
+            let mut delete = db.prepare(r#"DELETE FROM kv WHERE key = ? AND list_order = ?"#)?;
             delete.bind(1, key)?;
-            delete.bind(2, list_index)?;
+            delete.bind(2, list_order)?;
             delete.next()?;
         }
 
@@ -72,6 +77,8 @@ pub fn list_get_strings(key: &str) -> StringListValue {
                 string
             FROM
                 kv WHERE key = ?
+            ORDER BY
+                list_order ASC
         "#,
         )?;
         statement.bind(1, key)?;
@@ -80,6 +87,29 @@ pub fn list_get_strings(key: &str) -> StringListValue {
         });
 
         list
+    };
+
+    result.into()
+}
+
+#[marine]
+/// Remove a value from a list of strings. If the value is in several places, remove all of them.
+/// Returns an error on exceptions. Doesn't report if the value was actually removed.
+pub fn list_remove_value(key: &str, value: &str) -> UnitValue {
+    let conn = db();
+    let result: eyre::Result<()> = try {
+        // list_order == -1 when the key isn't a part of a list
+        let mut statement = conn.prepare(
+            r#"
+            DELETE FROM kv
+                WHERE key = ?
+                  AND string = ?
+                  AND list_order != -1
+        "#,
+        )?;
+        statement.bind(1, key)?;
+        statement.bind(2, value)?;
+        statement.next()?;
     };
 
     result.into()
@@ -160,4 +190,68 @@ mod tests {
         let get = spell.list_get_strings(key.into());
         assert_eq!(get.strings, &["хаха", "хаха", "haha"]);
     }
+
+    #[marine_test(config_path = "../../tests_artifacts/Config.toml")]
+    fn test_list_remove(spell: marine_test_env::spell::ModuleInterface) {
+        let key = "a";
+        let value = "value_to_remove";
+
+        let _ = spell.list_push_string(key.into(), "b".into());
+        let _ = spell.list_push_string(key.into(), value.into());
+        let _ = spell.list_push_string(key.into(), "в".into());
+        let _ = spell.list_push_string(key.into(), "p".into());
+        let _ = spell.list_push_string(key.into(), value.into());
+        let _ = spell.list_push_string(key.into(), "a".into());
+
+        let remove = spell.list_remove_value(key.into(), value.into());
+        assert!(remove.success, "remove failed {}", remove.error);
+
+        let get = spell.list_get_strings(key.into());
+        assert!(get.success, "list_get_strings failed {}", get.error);
+        assert_eq!(get.strings, vec!["b", "в", "p", "a"]);
+
+        let remove = spell.list_remove_value(key.into(), "not-in-list".into());
+        assert!(remove.success, "remove of non-existent values from the list must return ok");
+
+        let get = spell.list_get_strings(key.into());
+        assert!(get.success, "list_get_strings failed {}", get.error);
+        assert_eq!(get.strings, vec!["b", "в", "p", "a"], "must be the same after removing of absent value");
+
+        let _ = spell.set_string("str".into(), "val".into());
+        let remove = spell.list_remove_value("str".into(), "val".into());
+        assert!(remove.success, "remove of non-existent key-values from a list must return ok");
+
+        let get = spell.get_string("str".into());
+        assert!(!get.absent, "non-list value must not be removed");
+    }
+
+    #[marine_test(config_path = "../../tests_artifacts/Config.toml")]
+    fn test_list_remove_and_add(spell: marine_test_env::spell::ModuleInterface) {
+        let key = "a";
+        let value = "value_to_remove";
+        let value_first = "value_first";
+
+        let _ = spell.list_push_string(key.into(), value_first.into());
+        let _ = spell.list_push_string(key.into(), value.into());
+        let _ = spell.list_push_string(key.into(), "в".into());
+        let _ = spell.list_push_string(key.into(), "p".into());
+        let _ = spell.list_push_string(key.into(), value.into());
+        let _ = spell.list_push_string(key.into(), "a".into());
+
+        let _ = spell.list_remove_value(key.into(), value.into());
+
+        let _ = spell.list_push_string(key.into(), "n1".into());
+        let _ = spell.list_push_string(key.into(), "n2".into());
+
+        let get = spell.list_get_strings(key.into());
+        assert!(get.success, "list_get_strings failed {}", get.error);
+        assert_eq!(get.strings, vec![value_first, "в", "p", "a", "n1", "n2"]);
+
+        let _ = spell.list_remove_value(key.into(), value_first.into());
+        let get = spell.list_get_strings(key.into());
+        assert!(get.success, "list_get_strings failed {}", get.error);
+        assert_eq!(get.strings, vec!["в", "p", "a", "n1", "n2"]);
+
+    }
+
 }
