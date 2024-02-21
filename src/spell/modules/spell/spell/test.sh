447111c2cee1f44c177f40292e88bad0e164ad3b
Nick <gurinderu@gmail.com>
2024-02-22 02:18:27 +0300

    change layout

--- src/spell/modules/spell/spell/test.sh
+++ src/spell/modules/spell/spell/test.sh
@@ -5,17 +5,17 @@ set -o errexit
 set -o pipefail
 
 cd "$(dirname "$0")"
-
-rm -f /storage/spell.sqlite
+mkdir -p tests_artifacts/tmp
+mkdir -p tests_artifacts/storage
 
 # build spell.wasm
 marine build --release
-cp ../target/wasm32-wasi/release/spell.wasm tests_artifacts/
-mkdir -p tests_artifacts/tmp
+cp ../target/wasm32-wasi/release/spell.wasm tests_artifacts/storage
 
-if [[ ! -f "tests_artifacts/sqlite3.wasm" ]]; then
+
+if [[ ! -f "tests_artifacts/storage/qlite3.wasm" ]]; then
   # download SQLite 3
-  curl -L https://github.com/fluencelabs/sqlite/releases/download/sqlite-wasm-v0.18.2/sqlite3.wasm -o tests_artifacts/sqlite3.wasm
+  curl -L https://github.com/fluencelabs/sqlite/releases/download/sqlite-wasm-v0.18.2/sqlite3.wasm -o tests_artifacts/storage/sqlite3.wasm
 fi
 
 # run tests
